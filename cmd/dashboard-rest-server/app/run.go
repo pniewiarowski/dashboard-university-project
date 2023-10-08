f99ab9935791f35a19b0ec4e153bcac67173bac2
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2023-10-08 11:41:55 +0200

    Create base web application using Fiber.

--- cmd/dashboard-rest-server/app/run.go
+++ cmd/dashboard-rest-server/app/run.go
@@ -1,7 +1,18 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func Run(port int) {
-	fmt.Println("REST is running...")
+	app := fiber.New()
+	app.Use(logger.New())
+
+	rest := app.Group("/rest")
+	_ = rest.Group("/v1")
+
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
 }
